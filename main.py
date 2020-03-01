c12604f66ec4e1783ef2127c3beac321b93ce6ce
mac <cwx1568@163.com>
2020-03-01 21:58:56 +0800

    xx

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     https://github.com/tychxn/jd-assistant/wiki/1.-%E4%BA%AC%E4%B8%9C%E6%8A%A2%E8%B4%AD%E5%8A%A9%E6%89%8B%E7%94%A8%E6%B3%95
     """
 
-    sku_ids = '1178879'  # 商品id
+    sku_ids = '65881961548'  # 商品id
     area = '2_2824_51919'  # 区域id
     asst = Assistant()  # 初始化
     asst.login_by_QRcode()  # 扫码登陆
